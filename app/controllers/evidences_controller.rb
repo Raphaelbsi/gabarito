d1b39974545730081441243d4fc70eeac2f56790
Raphael Souza de Oliveira <oliveira_raphael@msn.com>
2023-02-22 15:07:54 -0300

    life cycle evidences

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class EvidencesController < ApplicationController
+  before_action :authorize
   before_action :set_evidence, only: %i[show update destroy]
   # GET /evidences
   def index
@@ -16,7 +17,31 @@ class EvidencesController < ApplicationController
 
   # POST Create Evidence /evidences
   def create
-    @evidence = Evidence.create(evidence_params)
+    evidence = Evidence.new(evidence_params)
+    evidence.user_id = @user.id
+    if evidence.save
+      render json: evidence, status: :created, location: evidence
+    else
+      render json: evidence.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /evidences/1
+
+  def update
+    if @evidence.update(evidence_params)
+      render json: @evidence, status: :accepted
+    else
+      render json: @evidence.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @evidence.destroy
+      render json: @evidence, status: :ok
+    else
+      render json: @evidence.errors, status: :unprocessable_entity
+    end
   end
 
   private
